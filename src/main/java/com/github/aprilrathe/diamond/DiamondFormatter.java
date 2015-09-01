93d1b912f1ed5da48e2af0872620d24e80f2a9ee
April Rathe <aprilrathe@gmail.com>
2015-09-01 08:22:19 -0400

    DiamondFormatter input 'A'

---
+++ src/main/java/com/github/aprilrathe/diamond/DiamondFormatter.java
@@ -0,0 +1,17 @@
+package com.github.aprilrathe.diamond;
+
+/**
+ * Hello world!
+ *
+ */
+public class DiamondFormatter
+{
+    public int calculateLayout(char endLetter) {
+        int spaces = 0;
+
+        if(endLetter == 'A') {
+            spaces = 0;
+        }
+        return spaces;
+    }
+}
