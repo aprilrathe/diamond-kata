90c07837c77fa21d85d9f980ad7ee1a5f117cd37
April Rathe <aprilrathe@gmail.com>
2015-09-03 08:47:31 -0400

    Fixed test scenarios. Refactored implementation.

--- src/main/java/com/github/aprilrathe/diamond/Diamond.java
+++ src/main/java/com/github/aprilrathe/diamond/Diamond.java
@@ -2,31 +2,68 @@ package com.github.aprilrathe.diamond;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import org.apache.commons.lang3.StringUtils;
 
 public class Diamond
 {
-    private static final char startLetter = 'A';
+    public static final char SPACER = '-';
+    public static final char START_LETTER = 'A';
 
     public static List<String> format(char endLetter) {
-        List<String> diamondFormat = new ArrayList<String>();
-
-        if(endLetter == startLetter) {
-            diamondFormat.add(String.valueOf(startLetter));
-            diamondFormat.add(String.valueOf(endLetter));
-        }
-        else if (endLetter == 'B') {
-            diamondFormat.add(" "+String.valueOf(startLetter));
-            diamondFormat.add(String.valueOf(endLetter));
-            diamondFormat.add(" "+String.valueOf(startLetter));
-        }
-        else if (endLetter == 'C') {
-            diamondFormat.add("  "+String.valueOf(startLetter));
-            diamondFormat.add(" " + String.valueOf((char)(startLetter + 1)));
-            diamondFormat.add(String.valueOf(endLetter));
-            diamondFormat.add(" " + String.valueOf((char)(startLetter + 1)));
-            diamondFormat.add("  "+String.valueOf(startLetter));
-        }
-        return diamondFormat;
+        List<String> diamondStructure = new ArrayList<String>();
+        List<String> diamondTop = createTopRows(endLetter);
+        List<String> diamondBottom = createBottomRows(diamondTop);
+        diamondStructure.addAll(diamondTop);
+        diamondStructure.addAll(diamondBottom);
+        return diamondStructure;
     }
 
+    private static List<String> createBottomRows(List<String> firstHalf) {
+        List<String> mirror = new ArrayList<String>(firstHalf);
+        Collections.reverse(mirror);
+        mirror.remove(0);
+        return mirror;
+    }
+
+    private static List<String> createTopRows(char endLetter) {
+        List<String> rows = new ArrayList<String>();
+        int numColumns = numberOfColumns(endLetter);
+        int numLetters = numberOfLetters(endLetter);
+
+        for(int i=0; i < numLetters; i++) {
+            char currentChar = (char)(START_LETTER + i);
+            rows.add(createRow(currentChar, i, numColumns, numLetters));
+        }
+        return rows;
+    }
+
+    private static String createRow(char currentLetter, int rowNumber, int numColumns, int numberOfLetters) {
+        String row = StringUtils.EMPTY;
+        int firstIndex = numColumns - numberOfLetters - rowNumber;
+        int middleIndex = numberOfLetters - firstIndex - 1;
+
+        row += createSpaceString(firstIndex);
+        row += currentLetter;
+        row += createSpaceString(middleIndex);
+
+        String mirror = StringUtils.reverse(row);
+        mirror = StringUtils.right(mirror, numberOfLetters - 1);
+        row += mirror;
+
+        return row;
+    }
+
+
+    private static int numberOfColumns(char endLetter) {
+        return (endLetter - START_LETTER) * 2 + 1;
+    }
+
+    private static int numberOfLetters(char endLetter) {
+        return endLetter - START_LETTER + 1;
+    }
+
+    private static String createSpaceString(int length) {
+        return StringUtils.repeat(SPACER, length);
+    }
 }
--- src/test/java/com/github/aprilrathe/diamond/DiamondTest.java
+++ src/test/java/com/github/aprilrathe/diamond/DiamondTest.java
@@ -2,51 +2,31 @@ package com.github.aprilrathe.diamond;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class DiamondTest {
 
     @Test
-    public void format_inputA_twoRowsNoSpaces()
+    public void format_inputA_oneRowsNoSpaces()
     {
-        char[] letters = {'A','A'};
-        int[] spaces = {0,0};
-        List<String> expected = createExpectedDiamondFormat(letters, spaces);
         List<String> actual = Diamond.format('A');
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals("A", actual.get(0));
     }
 
     @Test
     public void format_inputB_threeRowsOneSpaceANoSpaceB() {
-        char[] letters = {'A','B','A'};
-        int[] spaces = {1,0,1};
-        List<String> expected = createExpectedDiamondFormat(letters, spaces);
         List<String> actual = Diamond.format('B');
-        Assert.assertEquals(expected, actual);
+
+        Assert.assertEquals("-A-", actual.get(0));
+        Assert.assertEquals("B-B", actual.get(1));
     }
 
     @Test
-    public void format_inputC_fourRowsTwoSpacesAOneSpaceBNoSpaceC() {
-        char[] letters = {'A','B','C','B','A'};
-        int[] spaces = {2,1,0,1,2};
-        List<String> expected = createExpectedDiamondFormat(letters, spaces);
+    public void format_inputC_fiveRowsTwoSpacesAOneSpaceBNoSpaceC() {
         List<String> actual = Diamond.format('C');
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals("--A--", actual.get(0));
+        Assert.assertEquals("-B-B-", actual.get(1));
     }
 
-    private List<String> createExpectedDiamondFormat(char[] letters, int[] spaces) {
-        List<String> diamond = new ArrayList<String>();
-        for(int i = 0; i<letters.length ; i++) {
-            String diamondRow = "";
-            for(int j=0; j<spaces[i];j++) {
-                diamondRow +=" ";
-            }
-            diamondRow += letters[i];
-            diamond.add(diamondRow);
-        }
-        return diamond;
-    }
 }
